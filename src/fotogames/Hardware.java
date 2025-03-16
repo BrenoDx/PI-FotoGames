0337e379563293c754d46bb7ee5f2b059abffd4e
Breno Alves <breno.alvesdx@gmail.com>
2025-03-16 01:34:00 -0300

    Feat: primeira versão do projeto

---
+++ src/fotogames/Hardware.java
@@ -0,0 +1,51 @@
+package fotogames;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ *
+ * @author breno
+ */
+public class Hardware extends Produto{
+   Scanner entrada = new Scanner(System.in);
+   public Hardware(){
+          }
+    public Hardware(String _nomeProduto, String _fabricante, float _valor, int _qntdProduto, int _garantia){
+        super(_nomeProduto, _fabricante, _valor, _qntdProduto, _garantia);
+    }
+    
+    @Override
+     public void cadastroProduto(List<Produto> produtos){
+        System.out.println("*****************Fotogames******************");
+        System.out.println("---------Cadastro Hardware/Console----------");
+        System.out.println("Nome do produto:");
+        String produto = entrada.nextLine();
+        System.out.println("Fabricante:");
+        String fabricante = entrada.nextLine();
+        System.out.println("Valor do produto:");
+        float valor = entrada.nextFloat();
+        entrada.nextLine(); 
+        System.out.println("Quantidade no estoque:");
+        int qntdProduto = entrada.nextInt();
+        entrada.nextLine();
+        System.out.println("Garantia do Produto: (Informar em total de dias)");
+        int garantia = entrada.nextInt();
+        entrada.nextLine();
+        System.out.println("*********************************************");
+        
+        Hardware listaHardware = new Hardware(produto, fabricante, valor, qntdProduto, garantia);
+        produtos.add(listaHardware);
+            }
+     
+    @Override
+    public void exibirProduto(){
+        super.exibirProduto();
+         System.out.println("*************************************");
+     } 
+    
+    @Override
+    public void pesquisarProduto(){
+            }
+}
